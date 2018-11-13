1e74c4647066bdae4c506a58fdbef10dbd921bbc
vemonet <vincent.emonet@gmail.com>
2018-11-13 13:46:25 +0100

    create file for split operation

---
+++ src/main/java/nl/unimaas/ids/sparql/Split.java
@@ -0,0 +1,17 @@
+package nl.unimaas.ids.sparql;
+
+public class Split {
+
+	
+	// We select the quads we want to split
+	// Then we split the string based on a separator (, or | for example)
+	// Delete old quads
+	// Insert new quads (one for each value separated by the separator)
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
