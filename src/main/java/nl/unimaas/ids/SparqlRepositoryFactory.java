7e8fd6a73a6162b4510e458677c5dc02afe6829c
vemonet <vincent.emonet@gmail.com>
2019-08-08 18:31:23 +0200

    now get HTTPRepo using repo ID when URL ending with /statements is provided

--- src/main/java/nl/unimaas/ids/SparqlRepositoryFactory.java
+++ src/main/java/nl/unimaas/ids/SparqlRepositoryFactory.java
@@ -18,19 +18,23 @@ public class SparqlRepositoryFactory {
         	httpRepo.initialize();
         	return httpRepo;
 		} else {
-			try {
-				HTTPRepository httpRepo = new HTTPRepository(endpointUrl);
+			if (endpointUrl.endsWith("/statements") && endpointUrl.contains("/repositories/")) {
+				// If a URL to a RDF4J update repository with /statements is provided
+				// Then we parse the URL to get a HTTP repo from triplestore URL + repo ID
+				repositoryId = endpointUrl.substring(endpointUrl.indexOf("/repositories/") + 14, endpointUrl.indexOf("/statements"));
+				endpointUrl = endpointUrl.substring(0, endpointUrl.indexOf("/repositories/"));
+				HTTPRepository httpRepo = new HTTPRepository(endpointUrl, repositoryId);
 	        	httpRepo.setUsernameAndPassword(username, password);
 	        	httpRepo.initialize();
 	        	return httpRepo;
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
+			} else {
 				System.out.println("Triplestore is not a RDF4J server, using SPARQLRepository instead of HTTPRepository");
 				SPARQLRepository sparqlRepo = new SPARQLRepository(endpointUrl);
 	   	 		sparqlRepo.setUsernameAndPassword(username, password);
 	   	 		sparqlRepo.initialize();
 	   	 		return sparqlRepo;
 			}
+			//} catch (IllegalArgumentException e) {
 		}
 	} 
 }
